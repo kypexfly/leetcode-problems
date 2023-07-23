01f4ccd81a829a1c40390a73207dde2c9665028a
Ricardo Chu <92066360+kypexfly@users.noreply.github.com>
2023-07-23 09:29:44 -0500

    two sum solution O(n)

--- problems/01_two_sum.js
+++ problems/01_two_sum.js
@@ -1,4 +1,18 @@
+// Solution 2: O(n) time, O(n) space
 export const twoSum = (nums, target) => {
+  const obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    const diff = target - n;
+    if (diff in obj) return [obj[diff], i];
+    obj[n] = i;
+  }
+  throw new Error("No two sum solution");
+};
+
+// Solution 1: O(n2) time, O(1) space
+export const twoSum_n2 = (nums, target) => {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] == target) {
